26660f53d0fb07ef079f10ddffa7ef48976bff7b
ryo_kurosu <dtrjs.kurosu.0510@gmail.com>
2023-01-24 19:45:36 +0900

    add getting mail list

---
+++ access_ss.py
@@ -0,0 +1,65 @@
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from oauth2client.service_account import ServiceAccountCredentials
+
+UA_infra = "https://docs.google.com/spreadsheets/d/1NqGG-FQHLYsIPZRq-Ju0INEc-V12PXcLauyq103TmkM/edit#gid=686231199"
+
+def get_authenticated_service():
+    # # OAuth2認証を使用してGoogleアカウントにアクセス
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+    client = gspread.authorize(credentials)
+
+    return client
+
+
+def access_ss(client):
+    # # スプレッドシートのURLまたはIDを指定してスプレッドシートを開く
+    spreadsheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1NqGG-FQHLYsIPZRq-Ju0INEc-V12PXcLauyq103TmkM/edit#gid=686231199')
+
+    # # シート名またはインデックスでワークシートを取得
+    worksheet = spreadsheet.worksheet("【触らない】会員データ")
+
+    return worksheet
+
+
+def get_no_apply_entors():
+    client = get_authenticated_service()
+    worksheet = access_ss(client)
+    
+    
+    # # 全てのセルの値を取得し、dfに格納
+    all_data = worksheet.get_all_values()
+    df = pd.DataFrame(all_data[2:], columns=all_data[1])
+    df = df.drop(df.columns[20:], axis=1)
+
+    # mask = df.iloc[:, 16] == 'いいえ' # 16列目の「申請済み」が「はい」以外の行を抽出
+    # df_no_apply_list = df[mask]
+    mask = df.iloc[:, 16] == 'はい' # 16列目の「申請済み」が「はい」以外の行を抽出
+    df_no_apply_list = df[~mask]
+    df_no_apply_list = df_no_apply_list.reset_index(drop=True)
+
+    no_apply_entors = df_no_apply_list.iloc[:, 3].tolist()
+    print(no_apply_entors)
+
+    return no_apply_entors
+
+
+def output_mail_lsit(mail_list):
+    client = get_authenticated_service()
+    spreadsheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1NqGG-FQHLYsIPZRq-Ju0INEc-V12PXcLauyq103TmkM/edit#gid=686231199')
+    worksheet = spreadsheet.worksheet("mail_list")
+    worksheet.clear()
+    
+    for target in mail_list:
+        worksheet.append_row([target[0], target[1]])
+    
+    return
+
+
+if __name__ == '__main__':
+    get_no_apply_entors()
---
+++ get_addres.py
@@ -0,0 +1,138 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+import os
+from selenium.common.exceptions import NoSuchElementException
+import user_info
+
+SLEEP_TIME = 3
+
+admin_url = 'https://admin-v2.en-courage.com/login'
+user_mail = user_info.USER_MAIL
+user_pass = user_info.USER_PASS
+
+
+def access_entor_page():
+    browser  = webdriver.Chrome()
+    browser.get(admin_url)
+
+    ### 場所とアクションを指定
+    elem_usermail = browser.find_element(By.ID,"email")
+    elem_usermail.send_keys(user_mail)
+
+    elem_userpass = browser.find_element(By.ID,"password")
+    elem_userpass.send_keys(user_pass)
+
+    elem_login_btn = browser.find_elements(By.TAG_NAME, "button")[0]
+    elem_login_btn.click()
+    sleep(SLEEP_TIME) # NOTE: 数秒待機して、ページ推移の情報変化に耐える
+
+    elements_entor_tab = browser.find_element(By.XPATH, '/html/body/div/div[2]/div[1]/div[2]/a[3]')
+    elements_entor_tab.click()
+    sleep(SLEEP_TIME)
+
+    return browser
+
+
+def get_entor_list_by_conditions(browser):
+    # 条件設定
+    elem_target_year = browser.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div/div[1]/div[2]/div/div[6]/div/div/div/div[2]')
+    elem_target_year.click()
+    elem_status = browser.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div/div[1]/div[2]/div/div[6]/div/div/div[2]/ul/li[4]')
+    elem_status.click()
+    elem_interview_status = browser.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div/div[1]/div[2]/div/div[8]/div/div/div[1]/div[2]')
+    elem_interview_status.click()
+    elem_status = browser.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div/div[1]/div[2]/div/div[8]/div/div/div[2]/ul/li[3]')
+    elem_status.click()
+    sleep(SLEEP_TIME)
+
+    is_entor_list = True
+    entor_count = 0
+    mail_list = []
+    while is_entor_list:
+        entor_count += 1
+        try:
+            elements_entor_page = browser.find_element(By.XPATH, f'/html/body/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr[{entor_count}]/td[1]/div/a')
+        except NoSuchElementException:
+            elements_entor_page = browser.find_element(By.XPATH, f'/html/body/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr[{entor_count-1}]/td[1]/div/a')
+            
+            # elements_entor_pageの座標を取得
+            # 所定の位置までスクロールし隠れた要素を表示させる
+            
+            sleep(SLEEP_TIME*2)
+            try:
+                elements_entor_page = browser.find_element(By.XPATH, f'/html/body/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr[{entor_count}]/td[1]/div/a')
+            except NoSuchElementException:
+                is_entor_list = False
+                break
+        
+        elements_entor_page.click()
+        sleep(SLEEP_TIME)
+        
+        elem_entor_mail = browser.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div/div[2]/div[2]/div[1]/div[2]/div[8]')
+        entor_mail = elem_entor_mail.text
+        print(entor_mail)
+        mail_list.append(entor_mail)
+        browser.back()
+        sleep(SLEEP_TIME)
+
+    print(mail_list)
+
+    sleep(10)
+    browser.quit()
+
+    return mail_list
+
+
+def get_entor_list_by_list(browser, target_list):
+    mail_list = []
+    
+    for target in target_list:
+        elem_name_searchbox = browser.find_element(By.ID, 'name')
+        elem_name_searchbox.clear()
+        elem_name_searchbox.send_keys(target)
+        sleep(SLEEP_TIME)
+        try:
+            elem_entor_page = browser.find_element(By.XPATH, f'/html/body/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr/td[1]/div/a')
+            elem_entor_page.click()
+            sleep(SLEEP_TIME)
+            elem_entor_mail = browser.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div/div[2]/div[2]/div[1]/div[2]/div[8]')
+            entor_mail = elem_entor_mail.text
+            mail_list.append([target, entor_mail])
+            browser.back()
+            sleep(SLEEP_TIME)
+        except:
+            continue
+        
+    sleep(10)
+    browser.quit()
+
+    return mail_list
+
+
+# 面談み申込者をあぶり出してから、名前を入力して、メールアドレスを取得する
+def get_entor_list_by_name_list():
+    import access_ss
+    # no_apply_list = access_ss.main()
+    
+    return
+
+
+def main():
+    import access_ss
+    no_apply_list = access_ss.get_no_apply_entors()
+    
+    browser = access_entor_page()
+    mail_list = get_entor_list_by_list(browser, no_apply_list)
+    print(mail_list)
+    
+    access_ss.output_mail_lsit(mail_list)
+    
+    
+    return
+
+
+if __name__ == '__main__':
+    main()
+    
+    
